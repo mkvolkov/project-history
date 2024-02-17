ba6582c70871e7a89e4c6868940b7a528dc60683
Mikhail Volkov <mk.volkov34@gmail.com>
2024-02-17 09:35:57 +0300

    slice demo

--- main.go
+++ main.go
@@ -20,4 +20,12 @@ func main() {
 	var x float64 = 7.62
 	fmt.Printf("Size of float64 = %d\n", unsafe.Sizeof(x))
 	fmt.Printf("Size of float32 = %d\n", unsafe.Sizeof(float32(x)))
+
+	sl1 := make([]int, 6)
+	sl1[2] = 700
+
+	fmt.Println(sl1)
+	fmt.Println("size of sl1 = ", unsafe.Sizeof(sl1))
+	fmt.Println("length of sl1 = ", len(sl1))
+	fmt.Println("capacity of sl1 = ", cap(sl1))
 }
