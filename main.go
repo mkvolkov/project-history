ae1ea2f68b85e8d5e0d72c85ca577bf2144fbd7a
Mikhail Volkov <mk.volkov34@gmail.com>
2024-02-17 09:19:20 +0300

    arithm int demo

--- main.go
+++ main.go
@@ -4,4 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("Git Replace demo")
+
+	var a, b = 5, 7
+
+	fmt.Printf("5 + 7 = %d\n", a+b)
+	fmt.Printf("5 - 7 = %d\n", a-b)
+	fmt.Printf("5 * 7 = %d\n", a*b)
+	fmt.Printf("5 / 7 = %d\n", a/b)
 }
